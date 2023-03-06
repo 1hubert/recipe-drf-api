3665c07937915cc516b805057ab5ad0e7dd3fb26
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2023-03-06 22:34:09 +0100

    setup django admin

--- app/core/models.py
+++ app/core/models.py
@@ -15,11 +15,22 @@ class UserManager(BaseUserManager):
     # extra fields allow any number of keyword args
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
         # Because our manager is associated to a model, we need to access the model that we are associated with
         # self.model is the same as defining a new user class
         # The normalize email method is provided from BaseUserManager
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
